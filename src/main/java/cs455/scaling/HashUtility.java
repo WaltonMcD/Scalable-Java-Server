b4d873f231aeea1095bda12b462dc2a6769f4add
WaltonMcDclear <wa1ton21@yahoo.com>
2022-03-10 20:28:44 -0700

    Padding hash function plus server tweaks.

--- src/main/java/cs455/scaling/HashUtility.java
+++ src/main/java/cs455/scaling/HashUtility.java
@@ -14,7 +14,21 @@ public class HashUtility {
         } catch (NoSuchAlgorithmException e) {
             System.out.println("Error SHA1: " + e.getMessage());
         } 
-        return hashInt.toString(16); 
+        String hash = padWithZeros(hashInt.toString(16), 40);
+        return hash; 
+    }
+
+    public String padWithZeros(String hash, int length){
+        if (hash.length() == length) {
+            return hash;
+        }
+        StringBuilder sb = new StringBuilder();
+        while (sb.length() < length - hash.length()) {
+            sb.append('0');
+        }
+        sb.append(hash);
+    
+        return sb.toString();
     }
     
 }
