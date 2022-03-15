b05dbf7cbdd7984c91376ecb48c068743e1a8bb2
WaltonMcDclear <wa1ton21@yahoo.com>
2022-03-15 14:32:14 -0600

    basic timer

---
+++ src/main/java/cs455/scaling/Scheduler.java
@@ -0,0 +1,22 @@
+package cs455.scaling;
+
+import java.util.Timer;
+
+public class Scheduler implements Runnable{
+    private final int batchTime;
+
+    public Scheduler(int batchTime){
+        this.batchTime = batchTime;
+
+    }
+
+    @Override
+    public void run() {
+        Timer timer = new Timer();
+        ServerStats stats = new ServerStats();
+
+        timer.scheduleAtFixedRate(stats, 0L, 5300L);
+        
+    }
+    
+}
